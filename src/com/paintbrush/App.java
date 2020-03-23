deffbc830f961c3139a45c8783454a8c14eb40b4
Yuri Braga <yuricancelabraga@gmail.com>
2020-03-22 21:59:30 -0300

    Java Swing tutorial

--- src/com/paintbrush/App.java
+++ src/com/paintbrush/App.java
@@ -3,18 +3,26 @@ package com.paintbrush;
 import javax.swing.*;
 
 public class App {
-    private JPanel mainPnl;
+    private JPanel MainPnl;
+    private JPanel paintPnl;
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(App::createAndShowGUI);
+
     }
 
     private static void createAndShowGUI(){
         System.out.println("Created GUI on EDT? " + SwingUtilities.isEventDispatchThread());
         JFrame f = new JFrame("App");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.add(new PaintPnl());
+        f.add(new App().MainPnl);
         f.pack();
         f.setVisible(true);
     }
+
+    private void createUIComponents() {
+        paintPnl = new PaintPnl();
+    }
 }
+
+
