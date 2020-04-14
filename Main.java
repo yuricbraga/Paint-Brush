3fdf227389bf082b68e2d9624c1432f08500e7ae
Saul Gustavo Caldeira Melo <saulgustavomelo@gmail.com>
2020-04-14 13:15:19 -0300

    Adição de comentários

--- Main.java
+++ Main.java
@@ -3,11 +3,24 @@ import javax.swing.SwingUtilities;
 
 import components.MainPanel;
 
+/**
+* Classe principal do PaintBrush
+*
+* @author Ian
+* @author Saul Melo
+* @author Yuri
+* @since 04 de 2020 
+* @version 1.2
+*/
 public class Main{
   public static void main(String[] args) {
     SwingUtilities.invokeLater(Main::createAndShowGUI);
   }
 
+   /**
+   * Este método inicializa a janela principal, JFrame. Com as configuracoes
+   * de layoult pre-definidas.
+   */
   private static void createAndShowGUI(){
     System.out.println("Created GUI on EDT? " + SwingUtilities.isEventDispatchThread());
     JFrame f = new JFrame("Paint brush");
