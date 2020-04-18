613ac71f458666c49a6d5596f3c325ac9f044665
Ian Rodrigues dos Reis Paixão <36201596+IanPaixao@users.noreply.github.com>
2020-04-17 21:55:06 -0300

    Update Resize.java

--- helpers/Resize.java
+++ helpers/Resize.java
@@ -2,17 +2,42 @@ package helpers;
 
 import java.awt.*;
 
+/**
+ * Classe que implementa o algoritmo de resize
+ *
+ * @author Ian
+ * @author Saul Melo
+ * @author Yuri
+ * @since 04 de 2020
+ * @version 1.2 <extends> JPanel
+ */
+
 public class Resize {
   private int rows;
   private int cols;
   private Point start;
 
+  /**
+   * O Construtor da classe que realiza o resize de um objeto atravez de uma representacao (linhas e colunas) e o ponto inicial.
+   *
+   * @param int rows,
+   * @param int cols,
+   * @param Point start, ponto inicial
+   */
+  
   public Resize(int rows, int cols, Point start){
     this.rows = rows;
     this.cols = cols;
     this.start = start;
   }
 
+  /**
+   * Metodo  que realiza  a recuperção de uma operção resize.
+   *
+   * @param float constX
+   * @param float constY
+   */
+  
   public Point[][] getResizedCoordinates(float constX, float constY){
     Point[][] response = new Point[rows][cols];
 
